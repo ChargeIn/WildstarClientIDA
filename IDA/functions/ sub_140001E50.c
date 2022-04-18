34735c61e0dfbd681836d930787b910df9a6d1ed
Florian Plesker <florian.plesker@web.de>
2022-04-18 11:05:46 +0200

    Removed dublicates

--- IDA/functions/ sub_140001E50.c
+++
@@ -1,8 +0,0 @@
-#include "../winhttp.h"
-
-//----- (0000000140001E50) ----------------------------------------------------
-__int64 __fastcall sub_140001E50(__int64 a1)
-{
-	return (unsigned int)_InterlockedIncrement((volatile signed __int32*)(a1 + 8));
-}
-
